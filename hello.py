ce48cc73922575fb848de3e9a71c033e101b659d
ajherndz <andresjhernandez5@gmail.com>
2019-11-05 12:16:03 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -18,6 +18,6 @@ from datetime import datetime
 
 @route('/')
 def hello():
-    return "Hello Python for ENAGAS runs on IOx!  Now it is " + str(datetime.utcnow())
+    return "Hello Python for Andy runs on IOx!  Now it is " + str(datetime.utcnow())
 
 run(host='0.0.0.0', port=8080, debug=True)
